425d2af6494b9e396fff1daf644aea4927e893b6
Alaukik1911 <1911alaukik@gmail.com>
2023-06-27 00:28:06 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -12,15 +12,13 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import { verify } from "crypto";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
-//CONFIGURATIONS
-
+/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -34,30 +32,27 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-//FILE STORAGE
-
-const storage = multer.diskStorage({destination: function(req, file, cb){cb(null, "public/assets");
-},
-filename: function(req, file, cb){
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
     cb(null, file.originalname);
-}
+  },
 });
+const upload = multer({ storage });
 
-const upload = multer({storage});
-
-
-//ROUTES WITH FILES
+/* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-app.post("posts", verifyToken, upload.single("picture"), createPost);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-//ROUTES
+/* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-
-//MONGOOSE SETUP
-
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -67,12 +62,8 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    //ADDED DATA ONCE AND ADDED TO DATABASE
-    //User.insertMany(users);     
-    //Post.insertMany(posts);       
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
-
-
-  //just rechecking rework
-  
