349b1c164b12eb7f8989be59c4f0bf211b56ce2b
Alaukik1911 <1911alaukik@gmail.com>
2023-06-26 12:55:53 +0530

    ..

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,33 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import NavBar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+    const { _id, picturePath } = useSelector((state) => state.user);
+
     return (
         <Box>
             <NavBar />
+            <Box
+                width="100%"
+                padding="2rem 6%"
+                display={isNonMobileScreens ? "flex" : "block"}
+                gap="0.5rem"
+                justifyContent="space-between"
+            >
+                <box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+                    <UserWidget userId={_id} picturePath={picturePath} />
+                </box>
+                <Box
+                    flexBase ={isNonMobileScreens ? "26%" : undefined}
+                    mt={isNonMobileScreens ? undefined : "2rem"}
+                >
+
+                </Box>
+                {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+            </Box>
         </Box>
     );
 };
